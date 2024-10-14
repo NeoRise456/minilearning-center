1d906c7074e5473287d8ca14cdab9aca2d418a30
Juan Flores Moroco <pcsijflo@upc.edu.pe>
2024-10-14 17:43:32 -0500

    feat: add learning and profile bounded context

--- src/main/java/pe/edu/upc/center/platform/shared/domain/model/aggregates/AuditableAbstractAggregateRoot.java
+++ src/main/java/pe/edu/upc/center/platform/shared/domain/model/aggregates/AuditableAbstractAggregateRoot.java
@@ -11,6 +11,7 @@ import java.util.Date;
 
 @EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
+@Getter
 public class AuditableAbstractAggregateRoot<T extends AbstractAggregateRoot<T>> extends AbstractAggregateRoot<T> {
 
     @Id
